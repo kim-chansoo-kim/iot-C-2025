9769fd9148fa01f2b3b6f3c9eb7c905b00a45863
kim-chansoo <gagunge2@gmail.com>
2025-03-11 11:02:55 +0900

    중간커밋2025-03-11-1102

---
+++ Day01/HelloWorld.c
@@ -0,0 +1,8 @@
+#include <stdio.h>				// stdio == 파일이름, .h == 헤더파일
+/* 출력함수이름()입력			   C언어 함수 : 이름 뒤에()가 있으면 함수 | Python함수 : def func() : */
+int main(void)					// void(비어있는) ,int == 출력 | main() 함수는 어딜가던 무조건 있어야함
+{
+	printf("Hello World!!");	// 함수호출
+	return 0;					// 호출한 운영체재한테 0을 돌려줌 - 출력
+}
+// 함수의 형태 - 1.함수 선언, 2.함수 정의, 3.함수 호출
---
+++ Day01/name.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+	printf("KimChanSoo");
+	return 0;
+}
